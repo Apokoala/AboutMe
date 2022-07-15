fff4f195117be324e71a852162b477778c4f79c0
Apokoala <michael_gazaway@hotmail.com>
2022-07-15 02:54:15 -0400

    oof minimally viable product

---
+++ js/app.js
@@ -0,0 +1,53 @@
+'use strict'
+
+Question1();
+function Question1(){
+    let userinput = prompt ('Is Day[9] my favorite streamer? (type yes or y or no or n)');
+    while(userinput.toUpperCase != NO || N){
+        alert('I\'m pretty sure he is!');
+        // userinput = prompt('Is Day[9] my favorite streamer? (type yes or y or no or n)');
+    } 
+    console.log('Day[9] is my fav!')
+}
+
+Question2();
+function Question2(){
+    let userinput = prompt ('Is Sydney my dog? (type yes or y or no or n)');
+    while(userinput.toUpperCase != NO || N){
+        alert('That\'s not one of my puppos');
+        userinput = prompt('Is Sydney my dog? (type yes or y or no or n)');
+    } 
+    console.log('Sydney is my puppo!')
+}
+
+Question3();
+function Question3(){
+    let userinput = prompt ('Is Golden Girls one of my favorite shows? (type yes or y or no or n)');
+    while(userinput.toUpperCase != NO || N){
+        alert('nopers!');
+        userinput = prompt('Is Golden Girls one of my favorite shows? (type yes or y or no or n)');
+    } 
+    console.log('technically it is my bestest most favoritist show')
+}
+
+Question4();
+function Question4(){
+    let userinput = prompt ('Do I have two eyes? (type yes or y or no or n)');
+    while(userinput.toUpperCase != 2){
+        alert('¯\_(ツ)_/¯');
+        userinput = prompt('Do I have two eyes? (type yes or y or no or n)');
+    } 
+    console.log('for now, yes, 2')
+}
+
+Question4();
+function Question4(){
+    let userinput = prompt ('Is brandon sanderson my favorite author?');
+    while(userinput.toUpperCase != NO || N){
+        alert('WROOOOOOONG!');
+        userinput = prompt('Is brandon sanderson my favorite author?');
+    } 
+    console.log('his stuff is so good...so good!')
+}
+
+
