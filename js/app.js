7019cf10472098ec68b76bdcef48b5f3f87be1c0
Apokoala <michael_gazaway@hotmail.com>
2022-07-16 15:36:06 -0400

    update

--- js/app.js
+++ js/app.js
@@ -3,51 +3,63 @@
 Question1();
 function Question1(){
     let userinput = prompt ('Is Day[9] my favorite streamer? (type yes or y or no or n)');
-    while(userinput.toUpperCase != NO || N){
-        alert('I\'m pretty sure he is!');
-        // userinput = prompt('Is Day[9] my favorite streamer? (type yes or y or no or n)');
-    } 
-    console.log('Day[9] is my fav!')
+   while (userinput.toUpperCase() === 'NO' || userinput.toUpperCase() === 'N'){
+        console.log('Incorrect');
+        alert('Incorrect');
+        userinput = prompt ('Is Day[9] my favorite streamer? (type yes or y or no or n)');
+    }
+    console.log('Correct');
+    alert('Correct');
 }
 
 Question2();
 function Question2(){
     let userinput = prompt ('Is Sydney my dog? (type yes or y or no or n)');
-    while(userinput.toUpperCase != NO || N){
-        alert('That\'s not one of my puppos');
+    if (userinput.toUpperCase () === 'NO' || userinput.toUpperCase () === 'N'){
+        console.log('Sydney is my puppo!')
+        alert('She sure is!');
         userinput = prompt('Is Sydney my dog? (type yes or y or no or n)');
     } 
-    console.log('Sydney is my puppo!')
+    console.log('Correct');
+    alert('Correct');
 }
 
 Question3();
 function Question3(){
     let userinput = prompt ('Is Golden Girls one of my favorite shows? (type yes or y or no or n)');
-    while(userinput.toUpperCase != NO || N){
-        alert('nopers!');
+    if (userinput.toUpperCase() === 'NO' || userinput.toUpperCase() === 'N'){
+        console.log('technically it is my bestest, most favoritist, show')
+        alert('Darn tootin');
         userinput = prompt('Is Golden Girls one of my favorite shows? (type yes or y or no or n)');
     } 
-    console.log('technically it is my bestest most favoritist show')
+    console.log('Correct');
+    alert('Correct');
 }
 
 Question4();
 function Question4(){
     let userinput = prompt ('Do I have two eyes? (type yes or y or no or n)');
-    while(userinput.toUpperCase != 2){
+    while (userinput.toUpperCase() === 'NO' || userinput.toUpperCase() === 'N'){
+        console.log('Do you think I\'m a pirate')
         alert('¯\_(ツ)_/¯');
         userinput = prompt('Do I have two eyes? (type yes or y or no or n)');
     } 
-    console.log('for now, yes, 2')
+    console.log('Correct');
+    alert('Correct');
 }
 
 Question4();
 function Question4(){
-    let userinput = prompt ('Is brandon sanderson my favorite author?');
-    while(userinput.toUpperCase != NO || N){
+    let userinput = prompt ('Is Brandon Sanderson my favorite author?');
+    if (userinput.toUpperCase() === 'NO' || userinput.toUpperCase() === 'N'){
+        console.log('Think again')
         alert('WROOOOOOONG!');
         userinput = prompt('Is brandon sanderson my favorite author?');
     } 
-    console.log('his stuff is so good...so good!')
+    console.log('Correct');
+    alert('Correct');
 }
 
-
+let firstName = prompt ('Give me your name!');
+// console.log(document.getElementById("firstName"));
+document.getElementById("firstName").innerHTML = 'Hello, ' +firstName+ ', welcome to my page'
